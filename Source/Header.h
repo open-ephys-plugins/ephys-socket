bd4331af491ae991722441ecd0022a7e90032db2
Brandon Parks <bap@open-ephys.org>
2024-03-05 21:48:37 -0500

    Add Header class
    Add a simple Header class to house the variables that are transmitted prior to the data

---
+++ Source/Header.h
@@ -0,0 +1,27 @@
+#ifndef __EPHYSHEADERH__
+#define __EPHYSHEADERH__
+
+#include <VisualizerEditorHeaders.h>
+
+namespace EphysSocketNode
+{
+	enum Depth { U8, S8, U16, S16, S32, F32, F64 };
+
+	class Header
+	{
+	public:
+
+		Header();
+
+		Header(std::vector<std::byte>& header_bytes);
+
+		int offset;
+		int num_bytes;
+		Depth depth;
+		int element_size;
+		int num_samp;
+		int num_channels;
+	};
+}
+
+#endif
