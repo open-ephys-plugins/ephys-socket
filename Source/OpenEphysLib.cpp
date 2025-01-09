e8fcd4b52aeda21f2145817521b0d15546e15569
Anjal Doshi <anjaldoshi09@gmail.com>
2025-01-09 14:33:25 -0800

    Bump plugin version to 0.5.0

--- Source/OpenEphysLib.cpp
+++ Source/OpenEphysLib.cpp
@@ -38,7 +38,7 @@ extern "C" EXPORT void getLibInfo (Plugin::LibraryInfo* info)
 {
     info->apiVersion = PLUGIN_API_VER;
     info->name = "Ephys Socket";
-    info->libVersion = "0.4.0";
+    info->libVersion = "0.5.0";
     info->numPlugins = NUM_PLUGINS;
 }
 
